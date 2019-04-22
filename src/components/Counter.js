ba27308534abfea9bb042be43f00529808bb1629
ayunas <ayunas@gmail.com>
2019-04-22 16:27:33 -0500

    added react-dom the the package.json to update react-dom to the latest version as react 16.8.6 and app is resolving to the screen now

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { increment, decrement } from '../actions';
+import { INCREMENT, DECREMENT } from '../actions';
 
 class Counter extends Component {
     incrementIfOdd = () => {
@@ -56,4 +56,4 @@ const mapStateToProps = (state) => {
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
-export default connect(mapStateToProps, { increment, decrement })(Counter);
+export default connect(mapStateToProps, { INCREMENT, DECREMENT })(Counter);
